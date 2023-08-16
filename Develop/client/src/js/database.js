8c00d09e45703699fa6f1e56ee9d99708f41c99e
C12 <mcpartlandpatrick@gmail.com>
2023-08-16 15:16:15 -0400

    updated GET request in database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,11 +13,11 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async () => {
 const jateDb = await openDB('jate', 1);
 const tx = jateDb.transaction('jate', 'readwrite');
 const store = tx.objectStore('jate');
-const request = store.put({ id: id, value: content });
+const request = store.put({ id: id, value: value });
 const result = await request;
 console.log('Data saved to Jate', result);
 };
@@ -26,7 +26,7 @@ console.log('Data saved to Jate', result);
 export const getDb = async () => {
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
-  const tx = todosDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
